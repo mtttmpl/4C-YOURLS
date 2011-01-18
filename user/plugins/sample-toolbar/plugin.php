4a147f3c48feb2491c0b7f2c9ffce28f53283e4c
Matthew Temple <matt@mattytemple.com>
2011-01-18 15:46:32 +0000

    added html layer for logo and added ad code

--- user/plugins/sample-toolbar/plugin.php
+++ user/plugins/sample-toolbar/plugin.php
@@ -80,6 +80,34 @@ function ozh_toolbar_do( $args ) {
 </head>
 <body>
 <div id="yourls-bar">
+	<div id="toolbar-logo">
+	
+	</div>
+	
+	<div id="toolbar-ad">
+	<!--/* OpenX Javascript Tag v2.8.7 */-->
+	
+	<script type='text/javascript'><!--//<![CDATA[
+	   document.MAX_ct0 ='INSERT_CLICKURL_HERE';
+	
+	   var m3_u = (location.protocol=='https:'?'https://4c.to/ads/www/delivery/ajs.php':'http://4c.to/ads/www/delivery/ajs.php');
+	   var m3_r = Math.floor(Math.random()*99999999999);
+	   if (!document.MAX_used) document.MAX_used = ',';
+	   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+	   document.write ("?zoneid=1&amp;target=_blank&amp;block=1&amp;blockcampaign=1");
+	   document.write ('&amp;cb=' + m3_r);
+	   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+	   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+	   document.write ("&amp;loc=" + escape(window.location));
+	   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+	   if (document.context) document.write ("&context=" + escape(document.context));
+	   if ((typeof(document.MAX_ct0) != 'undefined') && (document.MAX_ct0.substring(0,4) == 'http')) {
+	       document.write ("&amp;ct0=" + escape(document.MAX_ct0));
+	   }
+	   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+	   document.write ("'><\/scr"+"ipt>");
+	//]]>--></script><noscript><a href='http://4c.to/ads/www/delivery/ck.php?n=af4c583b&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://4c.to/ads/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=af4c583b&amp;ct0=INSERT_CLICKURL_HERE' border='0' alt='' /></a></noscript>
+	</div>
 	
 	<div id="yourls-delicious">
 		<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like href="$url" layout="button_count" show_faces="false" width="450"></fb:like>
