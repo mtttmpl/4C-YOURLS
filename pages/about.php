e1e67e0490e091e33014a00d7d0adff150520250
mattytemple <matt@mattytemple.com>
2011-01-21 16:15:30 +0000

    started work on about page

--- pages/about.php
+++ pages/about.php
@@ -1,5 +1,12 @@
 <?php
+require_once( dirname(dirname(__FILE__)).'/includes/load-yourls.php' );
 
-echo "This is an About page. Its URL is simply ". YOURLS_SITE ."/about";
-
+yourls_html_head( 'tools', 'About' );
+yourls_html_logo();
 ?>
+
+	<div class="sub_wrap">
+	
+    4Charity are an organization who gives a high ammount of profit to charity. W</div>
+
+<?php yourls_html_footer(); ?>
